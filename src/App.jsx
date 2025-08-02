5db362dbf6e2114f9013a883ee355ee85afe96f6
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-08-02 12:41:53 +0530

    Basic Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import Home from './components/Home'
-import Header from './components/fixed/Header'
-import Footer from './components/fixed/Footer'
 
 const App = () => {
   return (
     <div className='w-full h-screen bg-gray-900 text-white flex flex-col items-center justify-center'>
-      <Header />
       <Home />
-      <Footer />
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,23 @@ import ImagePreview from './ImagePreview'
 
 const Home = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl lg:text-6xl font-bold font-[PP] tracking-wider border-b-2">
+        Enhancia
+      </h1>
+      <h1 className="text-lg lg:text-2xl font-semibold pt-1 lg:pt-0">
+        AI Image Enhancer
+      </h1>
+      <p className="text-lg">
+        Upload your Image and let AI enhamce it in seconds.
+      </p>
       <ImageUpload />
       <ImagePreview />
+      <p className="text-lg">
+        Powered by @PicWish
+      </p>
     </div>
-  )
+  );
 }
 
 export default Home
