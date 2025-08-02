cf7b016397c959f2c0ddb7a993fe5fd343e9610a
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-08-02 13:30:17 +0530

    Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,21 +4,21 @@ import ImagePreview from './ImagePreview'
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-4xl lg:text-6xl font-bold font-[PP] tracking-wider border-b-2">
-        Enhancia
-      </h1>
-      <h1 className="text-lg lg:text-2xl font-semibold pt-1 lg:pt-0">
-        AI Image Enhancer
-      </h1>
-      <p className="text-lg">
-        Upload your Image and let AI enhamce it in seconds.
-      </p>
+    <div className="w-full flex flex-col items-center justify-center">
+      <div className="w-full flex flex-col items-center justify-center">
+        <h1 className="text-4xl lg:text-6xl font-bold font-[PP] tracking-wider border-b-2">
+          Enhancia
+        </h1>
+        <h1 className="text-lg lg:text-2xl font-semibold pt-1 lg:pt-0 opacity-80">
+          AI Image Enhancer
+        </h1>
+        <p className="text-lg opacity-60">
+          Upload your Image and let AI enhamce it in seconds.
+        </p>
+      </div>
       <ImageUpload />
       <ImagePreview />
-      <p className="text-lg">
-        Powered by @PicWish
-      </p>
+      <p className="text-lg opacity-60">Powered by @PicWish</p>
     </div>
   );
 }
