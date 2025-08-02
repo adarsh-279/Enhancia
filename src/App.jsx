e766d5d55a224bfb865fe8235632039a4fd5ef49
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-08-02 14:21:10 +0530

    ImagePreview Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './components/Home'
 
 const App = () => {
   return (
-    <div className='w-full h-screen bg-gray-900 text-white flex flex-col items-center justify-center'>
+    <div className='min-w-screen h-screen bg-gray-900 text-white flex flex-col items-center justify-center pt-5'>
       <Home />
     </div>
   )
--- src/components/ImagePreview.jsx
+++ src/components/ImagePreview.jsx
@@ -2,8 +2,32 @@ import React from 'react'
 
 const ImagePreview = () => {
   return (
-    <div>ImagePreview</div>
-  )
+    <div className="w-full min-h-xl flex justify-center items-center flex-col lg:flex-row gap-5 lg:gap-8 pb-3">
+      {/* Original Image */}
+      <div className="bg-white shadow-xl rounded-xl overflow-hidden">
+        <h1 className="text-lg text-black font-semibold py-2 text-center bg-amber-200">
+          Original Image
+        </h1>
+        <img
+          src=""
+          alt="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+          className="w-60 h-60 lg:w-70 lg:h-70 object-cover"
+        />
+      </div>
+
+      {/* Enhanced Image */}
+      <div className="bg-white shadow-xl rounded-xl overflow-hidden">
+        <h1 className="text-lg text-black font-semibold py-2 text-center bg-blue-300">
+          Enhanced Image
+        </h1>
+        <img
+          src=""
+          alt="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+          className="w-60 h-60 lg:w-70 lg:h-70 object-cover"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ImagePreview
