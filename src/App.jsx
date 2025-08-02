913ec674896f32cd5c6defbd4633397d9ea34e15
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-08-02 12:07:49 +0530

    Basic Page Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Home from './components/Home'
+import Header from './components/fixed/Header'
+import Footer from './components/fixed/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='w-full h-screen bg-gray-900 text-white flex flex-col items-center justify-center'>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
   )
 }
 
