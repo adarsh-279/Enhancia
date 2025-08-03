9df364d2a0ebf421227c0953eee4a6584752c58f
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-08-03 13:58:33 +0530

    Loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div>Loading</div>
-  )
+    <div className="w-60 h-60 lg:w-70 lg:h-70 object-cover text-black flex items-center justify-center">
+      <div className='border-4 w-12 h-12 scale-120 rounded-full border-zinc-400 border-b-transparent animate-spin'></div>
+    </div>
+  );
 }
 
 export default Loading
