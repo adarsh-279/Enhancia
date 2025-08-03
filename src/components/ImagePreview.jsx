76a36c5f2cf7b49ec2f000be17a1d3062025c9fe
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-08-03 11:30:01 +0530

    ImagePreview Working

--- src/components/ImagePreview.jsx
+++ src/components/ImagePreview.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import Loading from "./Loading"
 
-const ImagePreview = () => {
+const ImagePreview = (props) => {
   return (
     <div className="w-full min-h-xl flex justify-center items-center flex-col lg:flex-row gap-5 lg:gap-8 pb-3">
       {/* Original Image */}
@@ -8,11 +9,17 @@ const ImagePreview = () => {
         <h1 className="text-lg text-black font-semibold py-2 text-center bg-amber-200">
           Original Image
         </h1>
-        <img
-          src=""
-          alt="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
-          className="w-60 h-60 lg:w-70 lg:h-70 object-cover"
-        />
+        {props.uploaded ? (
+          <img
+            src={props.uploaded}
+            alt=""
+            className="w-60 h-60 lg:w-70 lg:h-70 object-cover"
+          />
+        ) : (
+          <h1 className="w-60 h-60 lg:w-70 lg:h-70 object-cover text-black flex items-center justify-center">
+            No Image Uploaded
+          </h1>
+        )}
       </div>
 
       {/* Enhanced Image */}
@@ -20,11 +27,20 @@ const ImagePreview = () => {
         <h1 className="text-lg text-black font-semibold py-2 text-center bg-blue-300">
           Enhanced Image
         </h1>
-        <img
-          src=""
-          alt="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
-          className="w-60 h-60 lg:w-70 lg:h-70 object-cover"
-        />
+        {props.enhanced && !props.loading && (
+          <img
+            src={props.enhanced}
+            alt=""
+            className="w-60 h-60 lg:w-70 lg:h-70 object-cover"
+          />
+        )}
+
+        {props.loading ? <Loading /> :(
+          <h1 className="w-60 h-60 lg:w-70 lg:h-70 object-cover text-black flex items-center justify-center">
+            No Enhanced Image
+          </h1>
+        )}
+
       </div>
     </div>
   );
